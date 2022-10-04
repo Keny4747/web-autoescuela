a354c4fcebb1ba00c17860c004d9ae271607af54
Keny Pau Vas <keny_pauccarima@usmp.pegit config --local user.email keny-47@hotmail.com>
2022-10-04 15:40:55 -0500

    commit

--- src/main/java/com/auto/web/controller/PlanController.java
+++ src/main/java/com/auto/web/controller/PlanController.java
@@ -1,8 +1,11 @@
 package com.auto.web.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,33 +16,40 @@ import com.auto.web.service.IPlanService;
 @Controller
 @RequestMapping("/plan")
 public class PlanController {
-	
+
 	@Autowired
 	IPlanService planService;
-	
+
 	@GetMapping("/form")
 	public String form(Plan plan, Model model) {
-		
+
 		model.addAttribute("titulo", "Registro de Plan de estudios");
 		model.addAttribute("plan", plan);
 		return "plan/form";
 	}
-	
+
 	@PostMapping("/form")
-	public String create(Plan plan,Model model) {
+	public String create(@Valid Plan plan,BindingResult result ,Model model) {
+		
+
+		if (result.hasErrors()) {
+
+			model.addAttribute("plan", plan);
+			model.addAttribute("titulo", "Registro de Plan");
+			return "plan/form";
+		}
 		planService.create(plan);
-	
 		return "redirect:/plan/listar";
 	}
-	
+
 	@GetMapping("/listar")
 	public String form(Model model) {
-		
+
 		model.addAttribute("lista", planService.findAll());
-		model.addAttribute("titulo","Lista de plan de estudios");
+		model.addAttribute("titulo", "Lista de plan de estudios");
 		return "plan/listar";
 	}
-	
+
 	@RequestMapping(value = "/form/{id}")
 	public String editar(@PathVariable(value = "id") Integer id, Model model) {
 

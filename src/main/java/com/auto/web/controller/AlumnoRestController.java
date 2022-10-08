586929a314e81e41e316288fa6e0720bc3acfc7e
Keny Pau Vas <keny4796@gmail.com>
2022-10-08 18:56:56 -0500

    ...

--- src/main/java/com/auto/web/controller/AlumnoRestController.java
+++ src/main/java/com/auto/web/controller/AlumnoRestController.java
@@ -2,11 +2,8 @@ package com.auto.web.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import com.auto.web.models.Alumno;
-
 @RestController
 public class AlumnoRestController {
 
-	
 	
 }

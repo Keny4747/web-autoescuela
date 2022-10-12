818af1d87b3034ac48bd8aa4c2bc932d0374fab8
Keny Pau Vas <keny_pauccarima@usmp.pe>
2022-10-11 23:52:31 -0500

    ...

--- src/main/java/com/auto/web/repo/IUsuarioRepo.java
+++ src/main/java/com/auto/web/repo/IUsuarioRepo.java
@@ -1,9 +1,10 @@
 package com.auto.web.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.auto.web.models.Usuario;
-
+@Repository
 public interface IUsuarioRepo extends JpaRepository<Usuario, Long>{
 
 }
